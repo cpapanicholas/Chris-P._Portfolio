1e3702ce8ddf8eb376470ef0b0106376cafc636e
cpapanicholas <cpapanicholas@gmail.com>
2023-12-02 13:32:55 -0600

    touching up css styles.

--- src/main.jsx
+++ src/main.jsx
@@ -1,50 +1,58 @@
-import ReactDOM from 'react-dom/client';
-// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import ReactDOM from 'react-dom/client';
+// // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './style.css';
 
-// Bringing in the pages the router will use to conditionally show the appropriate views
+// // Bringing in the pages the router will use to conditionally show the appropriate views
+// import App from './App';
+// import Error from './sections/Error';
+// import Home from './sections/Home';
+// import Portfolio from './sections/Portfolio';
+// import AboutMe from './sections/AboutMe';
+// import Resume from './sections/Resume'
+
+// // Define the accessible routes, and which components respond to which URL
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     errorElement: <Error />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       {
+//         path: 'portfolio',
+//         element: <Portfolio />,
+//       },
+//       {
+//         path: 'aboutme',
+//         element: <AboutMe />,
+//       },
+//       {
+//         path: 'Resume',
+//         element: <Resume/>
+//       },
+//       {
+//         path: 'Contact',
+//         element: <Contact/>
+//       }
+//     ],
+//   },
+// ]);
+
+// // Render the RouterProvider component
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <RouterProvider router={router} />
+// );
+
+import ReactDOM from 'react-dom';
 import App from './App';
-import Error from './sections/Error';
-import Home from './sections/Home';
-import Portfolio from './sections/Portfolio';
-import AboutMe from './sections/AboutMe';
-import Resume from './sections/Resume'
+import './styles/App.css'; // Include the main App styles here
 
-// Define the accessible routes, and which components respond to which URL
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: 'portfolio',
-        element: <Portfolio />,
-      },
-      {
-        path: 'aboutme',
-        element: <AboutMe />,
-      },
-      {
-        path: 'Resume',
-        element: <Resume/>
-      },
-      {
-        path: 'Contact',
-        element: <Contact/>
-      }
-    ],
-  },
-]);
-
-// Render the RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <App />
 );
