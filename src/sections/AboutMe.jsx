c7082085f6a6e756c185b04cb8f2ca97a4a16c04
cpapanicholas <cpapanicholas@gmail.com>
2024-01-09 17:48:32 -0600

    added info for ABOUTME SECTION

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
       <h2>About Me</h2>
       <p>
         {/* Your introduction and personal information go here */}
-        I'm passionate about web development and creating innovative solutions. My journey in coding started...
+        I'm passionate about web development and creating innovative solutions. My journey in coding started in September of 2023.  I have completed and been certified by Northwestern University in full-stack web development.  My experience is limited to what I learned in class but since passing I have been strengthening my problem solving skills using code wars and I also have a coding group that I work with on a social media application. I am new to all of this but I know its a matter of time until I get my first job experience. I am asking for a entry level job in the web development field, but I am also open to internships as well.  I have been learning everything I can about web development in preparation for any interviews I land and for being the best I can be until I get my first job experience.  I have been learning this for almost a half year and I don't regret making the jump to take a course and learn something that will benefit my life and career.  I look forward to becoming a stronger coder and learning more.  
       </p>
     </div>
   );
