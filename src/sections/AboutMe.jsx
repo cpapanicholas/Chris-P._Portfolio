fa5166181564c11c7d4d10bc65d9ab8aa033f07b
cpapanicholas <cpapanicholas@gmail.com>
2024-05-29 10:37:24 -0500

    added Aboutme section improvement

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import '../App.css'; // Ensure to create and import a CSS file for styling
 
 const AboutMe = () => {
   return (
-    <div>
+    <div className="about-me-container">
       <h2>About Me</h2>
       <p>
-        {/* Your introduction and personal information go here */}
-        I'm passionate about web development and creating innovative solutions. My journey in coding started in September of 2023.  I have completed and been certified by Northwestern University in full-stack web development.  My experience is limited to what I learned in class but since passing I have been strengthening my problem solving skills using code wars and I also have a coding group that I work with on a social media application. I am new to all of this but I know its a matter of time until I get my first job experience. I am asking for a entry level job in the web development field, but I am also open to internships as well.  I have been learning everything I can about web development in preparation for any interviews I land and for being the best I can be until I get my first job experience.  I have been learning this for almost a half year and I don't regret making the jump to take a course and learn something that will benefit my life and career.  I look forward to becoming a stronger coder and learning more.  
+        I am passionate about web development and creating innovative solutions. My journey in coding started in September 2023, and I have been on a continuous learning path since then.
+      </p>
+      <p>
+        I completed a full-stack web development certification from Northwestern University. Though my experience is limited to what I learned in class, I have been actively strengthening my problem-solving skills through platforms like CodeWars. 
+      </p>
+      <p>
+        I am seeking an entry-level position in the web development field, but I am also open to internships. I have been diligently preparing for interviews and aim to excel in my first job experience. Despite being relatively new to this field, I am dedicated to learning and growing as a developer.
+      </p>
+      <p>
+        Taking the leap to learn web development has been one of the best decisions I have made, and I look forward to continually improving my skills.
+      </p>
+      
+      <h3>Skills</h3>
+      <ul>
+        <li>HTML, CSS, JavaScript</li>
+        <li>React, Node.js, Express</li>
+        <li>MongoDB/Mongoose, GraphQL, MySQL, NoSQL, Sequilize </li>
+        <li>Problem-solving and Debugging</li>
+        <li>Responsive Design and Server/Client side API  </li>
+        <li>Team Collaboration</li>
+      </ul>
+
+      <h3>Interests</h3>
+      <p>
+        Aside from coding I love to read, and play video games.  I spend a lot of time outside and at the gym as well; physical health is just as important as mental health. I also enjoy cooking and trying new foods.
       </p>
     </div>
   );
--- src/sections/Resume.jsx
+++ src/sections/Resume.jsx
@@ -4,26 +4,6 @@ const Resume = () => {
   // Replace 'your_resume_link' with the actual link to your resume
   const resumeLink = 'your_resume_link';
 
-  // Lists of front-end and back-end technologies
-  const frontEndTechnologies = [
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'jQuery',
-    'Responsive Design',
-    'React',
-    'Bootstrap',
-  ];
-
-  const backEndTechnologies = [
-    'APIs',
-    'Node.js',
-    'Express',
-    'MySQL, Sequelize',
-    'MongoDB, Mongoose',
-    'GraphQL',
-  ];
-
   return (
     <div>
       <h2>My Resume</h2>
@@ -33,24 +13,6 @@ const Resume = () => {
         <strong>Resume Link</strong>{' '}
         </a>
       </p>
-
-      <div>
-        <h3>Front-End Technologies</h3>
-        <ul>
-          {frontEndTechnologies.map((tech, index) => (
-            <li key={index}>{tech}</li>
-          ))}
-        </ul>
-      </div>
-
-      <div>
-        <h3>Back-End Technologies</h3>
-        <ul>
-          {backEndTechnologies.map((tech, index) => (
-            <li key={index}>{tech}</li>
-          ))}
-        </ul>
-      </div>
     </div>
   );
 };
